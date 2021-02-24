29542a90e069733968e014dffee28c81a71bef6e
panmissl <panmissl@163.com>
2021-02-24 23:51:55 +0800

    udpate

---
+++ cory-context-spring-boot-starter/src/main/java/com/cory/enums/CoryEnum.java
@@ -0,0 +1,13 @@
+package com.cory.enums;
+
+/**
+ * Created by Cory on 2021/2/22.
+ */
+public interface CoryEnum {
+
+    /** name，相当于code，存数据库 */
+    String name();
+
+    /** 在页面上显示的内容 */
+    String text();
+}
