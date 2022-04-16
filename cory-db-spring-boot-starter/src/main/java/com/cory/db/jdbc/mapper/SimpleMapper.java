7cf31e59e9243a3100f4f5b5aaa74589e1fefe7f
panliang <panmissl@163.com>
2022-04-16 19:53:01 +0800

    update

--- cory-db-spring-boot-starter/src/main/java/com/cory/db/jdbc/mapper/SimpleMapper.java
+++ cory-db-spring-boot-starter/src/main/java/com/cory/db/jdbc/mapper/SimpleMapper.java
@@ -18,26 +18,52 @@ public class SimpleMapper extends SimpleValueResultMapper {
         returnType.equals(String.class);
         */
         if (null == object) {
+            if (returnType.equals(Integer.class) || returnType.getName().equals("int")) {
+                return 0;
+            }
+            if (returnType.equals(Long.class) || returnType.getName().equals("long")) {
+                return 0;
+            }
+            if (returnType.equals(Double.class) || returnType.getName().equals("double")) {
+                return 0;
+            }
+            if (returnType.equals(Float.class) || returnType.getName().equals("float")) {
+                return 0;
+            }
+            if (returnType.equals(Short.class) || returnType.getName().equals("short")) {
+                return 0;
+            }
+            if (returnType.equals(Boolean.class) || returnType.getName().equals("boolean")) {
+                return false;
+            }
             return null;
         }
+
         if (object instanceof Number) {
             Number number = (Number) object;
-            if (returnType.equals(Integer.class)) {
+            if (returnType.equals(Integer.class) || returnType.getName().equals("int")) {
                 return number.intValue();
             }
-            if (returnType.equals(Long.class)) {
+            if (returnType.equals(Long.class) || returnType.getName().equals("long")) {
                 return number.longValue();
             }
-            if (returnType.equals(Double.class)) {
+            if (returnType.equals(Double.class) || returnType.getName().equals("double")) {
                 return number.doubleValue();
             }
-            if (returnType.equals(Float.class)) {
+            if (returnType.equals(Float.class) || returnType.getName().equals("float")) {
                 return number.floatValue();
             }
-            if (returnType.equals(Short.class)) {
+            if (returnType.equals(Short.class) || returnType.getName().equals("short")) {
                 return number.shortValue();
             }
         }
+
+        if (returnType.equals(Boolean.class) || returnType.getName().equals("boolean")) {
+            String objStr = object.toString();
+            //true/TRUE -> true, 非0：true，其它false
+            return objStr.equalsIgnoreCase("true") || !objStr.equals("0");
+        }
+
         return object;
     }
 }
